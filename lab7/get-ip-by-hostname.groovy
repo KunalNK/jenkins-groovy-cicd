a866a391fc4d7a50e4ad3de4b27baa5107e39083
Kunal K <knkillekar@gmail.com>
2022-04-05 00:50:28 +0530

    added lab6 & lab7

---
+++ lab7/get-ip-by-hostname.groovy
@@ -0,0 +1,4 @@
+#!groovy
+//-------------------------------------------------------------------
+def hostname = 'google.com'
+println InetAddress.getByName(hostname).address.collect { it & 0xFF }.join('.')
